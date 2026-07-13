f165d4a9a11a1c22151b1265831471f543cfb59c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-07-13 09:55:47 -0700

    load

--- load.go
+++ load.go
@@ -1,12 +1,7 @@
 package vocabtxt
 
 import (
-	"os"
 	"unsafe"
-
-	"codeberg.org/reiver/go-erorr"
-	"codeberg.org/reiver/go-field"
-	"golang.org/x/sys/unix"
 )
 
 const maxKey = 1073741824
@@ -59,40 +54,4 @@ func LoadFromBytes(destination *map[string]uint, bytes []byte) error {
 	}
 
 	return nil
-}
-
-// LoadFromBytes loads (WordPiece) "vocab.txt" data from a file into a map[string]uint.
-func LoadFromFile(destination *map[string]uint, filename string) error {
-	file, err := os.Open(filename)
-	if nil != err {
-		err = erorr.Wrap(err, "failed to open file",
-			field.String("file-name", filename),
-		)
-		return err
-	}
-	defer file.Close()
-
-	fileinfo, err := file.Stat()
-	if nil != err {
-		err = erorr.Wrap(err, "failed to stat file",
-			field.String("file-name", filename),
-		)
-		return err
-	}
-
-	size := fileinfo.Size()
-	if size <= 0 {
-		return nil
-	}
-
-	bytes, err := unix.Mmap(int(file.Fd()), 0, int(size), unix.PROT_READ, unix.MAP_SHARED)
-	if nil != err {
-		err = erorr.Wrap(err, "failed to mmap file",
-			field.String("file-name", filename),
-		)
-		return err
-	}
-	defer unix.Munmap(bytes)
-
-	return LoadFromBytes(destination, bytes)
 }
---
+++ load_unix.go
@@ -0,0 +1,45 @@
+package vocabtxt
+
+import (
+	"os"
+
+	"codeberg.org/reiver/go-erorr"
+	"codeberg.org/reiver/go-field"
+	"golang.org/x/sys/unix"
+)
+
+// LoadFromBytes loads (WordPiece) "vocab.txt" data from a file into a map[string]uint.
+func LoadFromFile(destination *map[string]uint, filename string) error {
+	file, err := os.Open(filename)
+	if nil != err {
+		err = erorr.Wrap(err, "failed to open file",
+			field.String("file-name", filename),
+		)
+		return err
+	}
+	defer file.Close()
+
+	fileinfo, err := file.Stat()
+	if nil != err {
+		err = erorr.Wrap(err, "failed to stat file",
+			field.String("file-name", filename),
+		)
+		return err
+	}
+
+	size := fileinfo.Size()
+	if size <= 0 {
+		return nil
+	}
+
+	bytes, err := unix.Mmap(int(file.Fd()), 0, int(size), unix.PROT_READ, unix.MAP_SHARED)
+	if nil != err {
+		err = erorr.Wrap(err, "failed to mmap file",
+			field.String("file-name", filename),
+		)
+		return err
+	}
+	defer unix.Munmap(bytes)
+
+	return LoadFromBytes(destination, bytes)
+}
