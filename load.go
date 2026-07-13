6049800076d8aea760a532427f5887b765eb2a6b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-07-13 09:32:35 -0700

    load

---
+++ load.go
@@ -0,0 +1,54 @@
+package vocabtxt
+
+import (
+	"unsafe"
+)
+
+const maxKey = 1073741824
+
+// LoadFromBytes loads (WordPiece) "vocab.txt data from a []byte into a map[string]uint.
+func LoadFromBytes(destination *map[string]uint, bytes []byte) error {
+	if nil == destination {
+		return ErrMapNil
+	}
+	if 0 < len(*destination) {
+		return ErrMapNotEmpty
+	}
+
+	if len(bytes) <= 0 {
+		return nil
+	}
+
+	// The keyBuffer length is chosen to try to be a multiple of a cache line
+	var keyBuffer [128]rune
+	var key []rune = keyBuffer[0:0]
+
+	// The line number of the vocab.txt data, starting with zero (0).
+	// I.e., zero-indexed (rather than one-indexed).
+	var index uint
+
+	var str string = unsafe.String(&bytes[0], len(bytes))
+	for _, r := range str {
+		if '\n' == r {
+			if 0 < len(key) {
+				if '\r' == key[len(key)-1] {
+					key = key[:len(key)-1]
+				}
+			}
+			(*destination)[string(key)] = index
+			key = keyBuffer[0:0]
+			index++
+			continue
+		}
+
+		key = append(key, r)
+		if maxKey < len(key) {
+			return ErrTokenTooBig
+		}
+	}
+	if 0 < len(key) {
+		(*destination)[string(key)] = index
+	}
+
+	return nil
+}
---
+++ loadfrombytes_test.go
@@ -0,0 +1,209 @@
+package vocabtxt_test
+
+import (
+	"testing"
+
+	"reflect"
+
+	"github.com/reiver/go-vocabtxt"
+)
+
+func TestLoadFromBytes(t *testing.T) {
+	tests := []struct{
+		Bytes []byte
+		Expected map[string]uint
+	}{
+		{
+			Expected: map[string]uint{},
+		},
+
+
+
+		{
+			Bytes:    []byte(
+				"apple",
+			),
+			Expected: map[string]uint{
+				"apple":0,
+			},
+		},
+		{
+			Bytes:    []byte(
+				"apple"+"\n",
+			),
+			Expected: map[string]uint{
+				"apple":0,
+			},
+		},
+		{
+			Bytes:    []byte(
+				"apple"+"\r\n",
+			),
+			Expected: map[string]uint{
+				"apple":0,
+			},
+		},
+
+
+
+		{
+			Bytes:    []byte(
+				"apple"+"\n"+
+				"banana",
+			),
+			Expected: map[string]uint{
+				"apple":0,
+				"banana":1,
+			},
+		},
+		{
+			Bytes:    []byte(
+				"apple"  +"\n"+
+				"banana" +"\n",
+			),
+			Expected: map[string]uint{
+				"apple":0,
+				"banana":1,
+			},
+		},
+
+
+
+		{
+			Bytes:    []byte(
+				"apple"+"\r\n"+
+				"banana",
+			),
+			Expected: map[string]uint{
+				"apple":0,
+				"banana":1,
+			},
+		},
+		{
+			Bytes:    []byte(
+				"apple"  +"\r\n"+
+				"banana" +"\r\n",
+			),
+			Expected: map[string]uint{
+				"apple":0,
+				"banana":1,
+			},
+		},
+
+
+
+		{
+			Bytes:    []byte(
+				"apple"  +"\n"+
+				"banana" +"\n"+
+				"cherry",
+			),
+			Expected: map[string]uint{
+				"apple":0,
+				"banana":1,
+				"cherry":2,
+			},
+		},
+		{
+			Bytes:    []byte(
+				"apple"  +"\n"+
+				"banana" +"\n"+
+				"cherry" +"\n",
+			),
+			Expected: map[string]uint{
+				"apple":0,
+				"banana":1,
+				"cherry":2,
+			},
+		},
+
+
+
+		{
+			Bytes:    []byte(
+				"apple"  +"\r\n"+
+				"banana" +"\r\n"+
+				"cherry",
+			),
+			Expected: map[string]uint{
+				"apple":0,
+				"banana":1,
+				"cherry":2,
+			},
+		},
+		{
+			Bytes:    []byte(
+				"apple"  +"\r\n"+
+				"banana" +"\r\n"+
+				"cherry" +"\r\n",
+			),
+			Expected: map[string]uint{
+				"apple":0,
+				"banana":1,
+				"cherry":2,
+			},
+		},
+
+
+
+		{
+			Bytes:    []byte(
+				"the"  +"\n"+
+				"of"   +"\n"+
+				"and"  +"\n"+
+				"in"   +"\n"+
+				"to"   +"\n"+
+				"was"  +"\n"+
+				"he"   +"\n"+
+				"is"   +"\n"+
+				"as"   +"\n"+
+				"for"  +"\n"+
+				"on"   +"\n"+
+				"with" +"\n"+
+				"that" +"\n"+
+				"it"   +"\n"+
+				"his"  +"\n"+
+				"by"   +"\n",
+			),
+			Expected: map[string]uint{
+				"the":0,
+				"of":1,
+				"and":2,
+				"in":3,
+				"to":4,
+				"was":5,
+				"he":6,
+				"is":7,
+				"as":8,
+				"for":9,
+				"on":10,
+				"with":11,
+				"that":12,
+				"it":13,
+				"his":14,
+				"by":15,
+			},
+		},
+	}
+
+	for testNumber, test := range tests {
+		var actual map[string]uint = map[string]uint{}
+
+		err := vocabtxt.LoadFromBytes(&actual, test.Bytes)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error but actually got one.", testNumber)
+			t.Logf("BYTES:\n%s", test.Bytes)
+			continue
+		}
+
+		expected := test.Expected
+
+		if !reflect.DeepEqual(expected, actual) {
+			t.Errorf("For test #%d, the actual 'map' it not what was expected.", testNumber)
+			t.Logf("EXPECTED\n%#v", expected)
+			t.Logf("ACTUAL:\n%#v", actual)
+			t.Logf("BYTES:\n%s", test.Bytes)
+			continue
+		}
+	}
+}
